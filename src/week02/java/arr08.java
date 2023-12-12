68064aa3b96aeb98e8657ccab1059240d648f2d4
kuma0112 <lsj23871@gmail.com>
2023-12-12 23:22:55 +0900

    전부 삭제

--- src/week02/java/arr08.java
+++ src/week02/java/arr08.java
@@ -8,10 +8,6 @@ import java.util.TreeSet;
 public class arr08 {
     public static void main(String[] args) {
         Set<Integer> integers = new HashSet();
-        //추가추가!!
-        //안녕하세요 신지님 거긴 행복하신가요??
-        integers.add(3);
-        //안녕하세요 신지님 거긴 행복하신가요??
-        //훈 브런치 생성
+
     }
 }
