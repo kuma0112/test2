b1f9b8484448a72417dc780669f68612ddb00ef9
kuma0112 <lsj23871@gmail.com>
2023-12-12 23:54:53 +0900

    dddd

--- src/week02/java/arr08.java
+++ src/week02/java/arr08.java
@@ -7,7 +7,8 @@ import java.util.TreeSet;
 
 public class arr08 {
     public static void main(String[] args) {
-       //2주 뒤에 크리스마스!!!
+       //2주 뒤에 크리스마스!!
+        System.out.println('1');
 
     }
 }
