bd7b0934a760d31dd2421579157ebcdc8d73fc21
kuma0112 <lsj23871@gmail.com>
2023-12-12 21:30:07 +0900

    Initial commit

---
+++ src/week02/java/arr08.java
@@ -0,0 +1,16 @@
+package week02.java;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class arr08 {
+    public static void main(String[] args) {
+        Set<Integer> integers = new HashSet();
+        integers.add(3);
+
+        Integer get = integers.get(3);
+
+    }
+}
